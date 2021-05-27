2aa2b5e2fb584db6ea3de0b81655afd67b5913db
Jota1985 <jjumana183@yahoo.com>
2021-05-27 13:09:05 +0000

    ver01

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let Numeros = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let Cartas = [
+  { color: "black", simbolo: "♠" },
+  { color: "Red", simbolo: "♥" },
+  { color: "black", simbolo: "♣" },
+  { color: "Red", simbolo: "♦" }
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const Numero = Numeros[Math.floor(Math.random() * 13)];
+const Carta = Cartas[Math.floor(Math.random() * 4)];
+
+document.getElementById("t-icon").innerHTML = Carta.simbolo;
+document.getElementById("t-icon").style.color = Carta.color;
+document.getElementById("number").innerHTML = Numero;
+document.getElementById("number").style.color = Carta.color;
+document.getElementById("b-icon").innerHTML = Carta.simbolo;
+document.getElementById("b-icon").style.color = Carta.color;
